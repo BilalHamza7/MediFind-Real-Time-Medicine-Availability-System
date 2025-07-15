df9d45da05e93df1de3490b0e6ba972f38058aae
Bilal Hamza <131806480+BilalHamza7@users.noreply.github.com>
2025-07-15 17:15:39 +0530

    updated folder

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,20 +1,12 @@
-<<<<<<< HEAD
-=======
-import React from 'react'
->>>>>>> f523a429efe69590f9f868ee1ea9f7284c43dccc
 import './App.css'
 
 function App() {
 
   return (
     <>
-<<<<<<< HEAD
       <div>
         
       </div>
-=======
-      
->>>>>>> f523a429efe69590f9f868ee1ea9f7284c43dccc
     </>
   )
 }
