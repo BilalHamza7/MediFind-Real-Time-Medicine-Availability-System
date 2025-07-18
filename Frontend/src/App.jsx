3f645161425e4cf09582adc9ec49e143c7249dc6
Bilal Hamza <131806480+BilalHamza7@users.noreply.github.com>
2025-07-18 22:42:13 +0530

    test

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,12 +2,9 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import './App.css'
 import Userlogin from './Auth/UserAuth/Userlogin'
 import PharmacistLogin from './Auth/PharmacistAuth/PharmacistLogin'
-<<<<<<< HEAD
 import PharmacistRegistration from './Auth/PharmacistAuth/PharmacistRegistration'
-=======
 import NavBar from './Components/NavBar'
 import UserRegistration from './Auth/UserAuth/UserRegistration'
->>>>>>> a44a07e8852c5de8b79197add075cdda2609c238
 
 function App() {
 
