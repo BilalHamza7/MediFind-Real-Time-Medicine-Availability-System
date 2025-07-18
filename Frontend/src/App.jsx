49b3bacaea4295a2abf47987e5f1fcff1ff7189e
Bilal Hamza <131806480+BilalHamza7@users.noreply.github.com>
2025-07-18 18:28:33 +0530

    completed responsive pharmacist sign up page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,6 +2,7 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import './App.css'
 import Userlogin from './Auth/UserAuth/Userlogin'
 import PharmacistLogin from './Auth/PharmacistAuth/PharmacistLogin'
+import PharmacistRegistration from './Auth/PharmacistAuth/PharmacistRegistration'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path='/userlogin' element={<Userlogin />} />
         <Route path='/pharmacistLogin' element={<PharmacistLogin />} />
+        <Route path='/pharmacistRegistration' element={<PharmacistRegistration />} />
       </Routes>
     </Router>
   )
