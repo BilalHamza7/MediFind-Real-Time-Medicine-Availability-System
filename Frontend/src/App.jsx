a44a07e8852c5de8b79197add075cdda2609c238
Mohamed Shafran <shafransheikh@gmail.com>
2025-07-18 17:16:10 +0530

    complete  user registration ui

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,13 +2,17 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import './App.css'
 import Userlogin from './Auth/UserAuth/Userlogin'
 import PharmacistLogin from './Auth/PharmacistAuth/PharmacistLogin'
+import NavBar from './Components/NavBar'
+import UserRegistration from './Auth/UserAuth/UserRegistration'
 
 function App() {
 
   return (
     <Router>
+      <NavBar />
       <Routes>
         <Route path='/userlogin' element={<Userlogin />} />
+        <Route path='/userRegistration' element={<UserRegistration />} />
         <Route path='/pharmacistLogin' element={<PharmacistLogin />} />
       </Routes>
     </Router>
